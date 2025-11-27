9ea8b4458408678b5dab4cdbddc1a3755d7b53ff
unknown <nataliapalladinog@gmail.com>
2025-11-27 18:58:13 -0400

    termine delete

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', listproducts.as_view(), name='lista_productos'),
     path("product/<int:pk>/", detailproduct.as_view(), name="detalle_producto"),
+    path('create/', PijamaCreateView.as_view(), name='pijama_create'),
+    path('product/<int:pk>/update/', PijamaUpdateView.as_view(), name='pijama_update'),
+    path('product/<int:pk>/delete/', PijamaDeleteView.as_view(), name='pijama_delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView as listView, DetailView
+from django.views.generic import ListView as listView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Pijama
 
 
@@ -16,4 +17,32 @@ class detailproduct(DetailView):
     context_object_name = 'producto'
     
 
-
+class PijamaCreateView(CreateView):
+    model = Pijama
+    template_name = 'create_product.html'
+    success_url = reverse_lazy('lista_productos')  # Ajusta este nombre de URL
+    fields = [
+        'nombre', 
+        'descripcion', 
+        'precio', 
+        'imagen', 
+        
+    ]
+    
+class PijamaUpdateView(UpdateView):
+    model = Pijama
+    template_name = 'create_product.html'
+    success_url = reverse_lazy('lista_productos')  # Ajusta este nombre de URL
+    fields = [
+        'nombre', 
+        'descripcion', 
+        'precio', 
+        'imagen', 
+        
+        'disponible'
+    ]
+    
+class PijamaDeleteView(DeleteView):
+    model = Pijama
+    template_name = 'delete_product.html'
+    success_url = reverse_lazy('lista_productos')
